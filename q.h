eb1b38b4c2bb74bbe6bdd6bfa1177b709f7f8520
JJ Robertson <jjrob13@gmail.com>
2015-10-06 01:47:31 +0000

    Add additional documentation.

--- q.h
+++ q.h
@@ -1,8 +1,31 @@
+/*
+Project:
+CSE 430 Project 1
+
+Semester:
+Fall 2015
+
+Instructor:
+Partha Dasgupta
+
+Authors:
+Nathaniel Mendoza
+John Robertson
+
+
+*/
+
 #ifndef Q_H
 #define Q_H
+/*
+////////////////////////
+START structure and type declarations
+
+
+////////////////////////
+*/
 
 typedef enum __QUEUE_STATUS {Q_SUCCESS, Q_FAILURE} QUEUE_STATUS;
-
 
 struct __Node{
 	struct __Node *next, *prev;
@@ -16,16 +39,93 @@ typedef struct __Queue{
 	int count;
 } Queue;
 
+/*
+////////////////////////
+END structure and type declarations
+
+
+////////////////////////
+*/
+
+/*/////////////////////////////////////////////////////////////////////////////// */
+
+/*
+////////////////////////
+START function declarations
+
+
+////////////////////////
+*/
 
 QUEUE_STATUS InitQueue(Queue *q);
-
-
 Node * NewItem(void);
-
-
 QUEUE_STATUS FreeItem(Node *item);
-
-
 QUEUE_STATUS AddQueue(Queue *q, Node *n);
+/*
+////////////////////////
+END function declarations
+
+
+////////////////////////
+*/
+
+/*/////////////////////////////////////////////////////////////////////////////// */
+
+/*
+////////////////////////
+START function definitions
+
+
+////////////////////////
+*/
+
+QUEUE_STATUS
+InitQueue(Queue *q){
+	q->count = 0;
+	return Q_SUCCESS;
+}
+
+
+Node *
+NewItem(){
+	return (Node *) malloc(sizeof(Node));
+}
+
+
+QUEUE_STATUS
+FreeItem(Node *item){
+	free(item);
+	return Q_SUCCESS;
+}
+
+
+QUEUE_STATUS
+AddQueue(Queue *q, Node *n){
+	//first item
+	if(q->count == 0){
+		q->head = n;
+		q->tail = n;
+	}else{
+		q->tail->next = n;
+		n->prev = q->tail;
+		q->tail = n;
+	}
+	
+	//always increment count
+	q->count++;
+	//always point head.prev to tail and tail.next to head
+	q->head->prev = q->tail;
+	q->tail->next = q->head;
+
+	return Q_SUCCESS;
+}
+
+/*
+////////////////////////
+END function definitions
+
+
+////////////////////////
+*/
 
 #endif
