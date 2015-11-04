27715c5cd0defdaa664c4d725c844fb5d8c9a7cb
Nathaniel <namendoz@asu.edu>
2015-11-03 18:24:07 -0700

    + Producer and Consumer implementation (plase work Git)

--- sem_test.c
+++ sem_test.c
@@ -1,27 +1,64 @@
 #include "sem.h"
+#include <time.h>
 
 
 /* thread routines */
 void t1(void);
 void t2(void);
-void producer(void);
-void consumer(void);
+void producer1(void);
+void consumer1(void);
+void producer2(void);
+void consumer2(void);
 /* int to be modified by both routines. */
 int shared_int;
 Semaphore *sem;
 
+
+/******Producer Consumer (Method 2)*********/
+Semaphore *empty, *full, *mutex;
+#define N 100
+int buffer[N];
+int in, out;
+
+/***************/
+
+
 int main(int argc, char ** argv){
 	/* We need to init the global thread queue */
 	runQ = (Queue*) malloc(sizeof(Queue));
-	sem = (Semaphore*) malloc(sizeof(Semaphore));
-	
+		
 	InitQueue(runQ);
+	
+	/* Method 1 Setup
+	sem = (Semaphore*) malloc(sizeof(Semaphore));
+
 	InitSem(sem, 1);
 	
 	shared_int = 0;
 	
 	start_thread(t1);
 	start_thread(t2);
+	*/
+
+	/* Method 2 - producer consumer setup */
+	srand(time(NULL));
+
+	in = 0;
+	out = 0;
+
+	empty = (Semaphore*) malloc(sizeof(Semaphore));
+	full = (Semaphore*) malloc(sizeof(Semaphore));
+	mutex = (Semaphore*) malloc(sizeof(Semaphore));
+
+	InitSem(empty, N);
+	InitSem(full, 0);
+	InitSem(mutex, 1);
+
+	start_thread(producer1);
+	start_thread(producer2);
+	start_thread(consumer1);
+	start_thread(consumer2);
+
 
 	/* start pulling threads from the runQ */	
 	run();
@@ -60,17 +97,74 @@ t2(){
 	}
 }
 
-void
-producer()
-{
 
+
+void
+producer1()
+{
+	while(1){
+		int item = rand();
+		P(empty);
+		P(mutex);
+		buffer[in] = item;
+		printf("Producer 1 places %d in the buffer at index %d\n", item, in);
+		in = (in + 1) % N;
+		
+		V(mutex);
+		V(full);
+		sleep(1);
+	}
 
 }
 
 
 void
-consumer()
+consumer1()
 {
-
+	while(1){
+		P(full);
+		P(mutex);
+		int item = buffer[out];
+		printf("Consumer 1 removes %d from the buffer at index %d\n", item, out);
+		out = (out + 1) % N;
+		V(mutex);
+		V(empty);
+		sleep(1);
+	}
 
 }
+
+void
+producer2()
+{
+	while(1){
+		int item = rand();
+		P(empty);
+		P(mutex);
+		buffer[in] = item;
+		printf("Producer 2 places %d in the buffer at index %d\n", item, in);
+		in = (in + 1) % N;
+		V(mutex);
+		V(full);
+		sleep(1);
+	}
+
+}
+
+
+void
+consumer2()
+{
+	while(1){
+		P(full);
+		P(mutex);
+		int item = buffer[out];
+		printf("Consumer 2 removes %d from the buffer at index %d\n", item, out);
+		out = (out + 1) % N;
+		V(mutex);
+		V(empty);
+		sleep(1);
+	}	
+
+}
+
